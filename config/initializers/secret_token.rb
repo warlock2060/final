46cea846d72a1e6c64cdee3951be042ead4078af
warlock2060 <warlock2060@gmail.com>
2013-06-13 10:35:18 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Final::Application.config.secret_token = '7b18e00c3bf946a42eb85b579fb615ad3d602336ccfa7d2e750daf38eed09b8d05766834f75dcc56960544bf1b38e41de389125ed41512d8b439fd8750f6a8b9'
